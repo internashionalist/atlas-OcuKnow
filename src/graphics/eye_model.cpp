3c61083e2ddc75b12eb89c87ac2b4ba0deec7724
Ariel Lopez <alexandrea.lopez@atlasschool.com>
2025-04-17 10:16:10 -0500

    cmakefile and erased dupoe decalaration

--- src/graphics/eye_model.cpp
+++ src/graphics/eye_model.cpp
@@ -1,15 +1,15 @@
 #include <QPixmap>
 #include <QLabel>
+#include <QDebug>
 
 QPixmap pixmap(":/images/half image eye.jpg");
-QPixmap pixmap = QPixmap::frontImage(image);
-    ui->label->setPixmap(pixmap);
+ui->label->setPixmap(pixmap);
 
 if (pixmap.isNull())
     {
         qDebug() << "Error";
     } else {
-    ui->label->setPixmap(pixmap);
-    qDebug() << "Success";
+    	ui->label->setPixmap(pixmap);
+    	qDebug() << "Success";
     }
 
