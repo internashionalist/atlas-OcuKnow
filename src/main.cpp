5ea2e778781355ef3a958f121b62621d7a50eb30
Stephen Newby <164127736+TheSnewby@users.noreply.github.com>
2025-04-21 18:44:52 -0500

    Changed Name, Ready to Merge
    Changed all mentions of mainwindow to quiz including class and header.
    
    adjusted the CMakeList.txt to work across multiple formats (i hope) and made it more cmake-ish.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <QApplication>
 #include "UI/introwidget.h"
+#include "logic/quiz.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	IntroWidget intro;
-	intro.resize(2000, 300);
-	intro.show();
+    // Quiz q; //starts directly with Quiz (for quiz debugging)
+    // q.show();
+
+    IntroWidget intro;
+    intro.resize(2000, 300);
+    intro.show();
 
 	return app.exec();
 }
