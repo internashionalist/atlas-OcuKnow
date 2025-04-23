ad9909790f57a032e8bbc8f46d923b0afbeafdc1
“internashionalist” <aylor.thames@atlasschool.com>
2025-04-22 22:35:07 -0500

    reorg final

--- src/build/CMakeFiles/OcuKnow.dir/cmake_clean.cmake
+++ src/build/CMakeFiles/OcuKnow.dir/cmake_clean.cmake
@@ -2,21 +2,10 @@ file(REMOVE_RECURSE
   "CMakeFiles/OcuKnow_autogen.dir/AutogenUsed.txt"
   "CMakeFiles/OcuKnow_autogen.dir/ParseCache.txt"
   "OcuKnow_autogen"
-  "CMakeFiles/OcuKnow.dir/OcuKnow_autogen/EWIEGA46WW/qrc_resources.cpp.o"
-  "CMakeFiles/OcuKnow.dir/OcuKnow_autogen/EWIEGA46WW/qrc_resources.cpp.o.d"
   "CMakeFiles/OcuKnow.dir/OcuKnow_autogen/mocs_compilation.cpp.o"
   "CMakeFiles/OcuKnow.dir/OcuKnow_autogen/mocs_compilation.cpp.o.d"
-  "CMakeFiles/OcuKnow.dir/UI/introwidget.cpp.o"
-  "CMakeFiles/OcuKnow.dir/UI/introwidget.cpp.o.d"
-  "CMakeFiles/OcuKnow.dir/logic/quiz.cpp.o"
-  "CMakeFiles/OcuKnow.dir/logic/quiz.cpp.o.d"
-  "CMakeFiles/OcuKnow.dir/main.cpp.o"
-  "CMakeFiles/OcuKnow.dir/main.cpp.o.d"
   "OcuKnow"
   "OcuKnow.pdb"
-  "OcuKnow_autogen/EWIEGA46WW/qrc_resources.cpp"
-  "OcuKnow_autogen/mocs_compilation.cpp"
-  "OcuKnow_autogen/timestamp"
 )
 
 # Per-language clean rules from dependency scanning.
